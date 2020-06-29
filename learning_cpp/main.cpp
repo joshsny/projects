2bd0e7acca2ff3b9574a72a5b507be2b9851659f
Joshua Snyder <js@joshua.pl>
2020-06-29 11:51:50 +0100

    random updates

---
+++ learning_cpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Martha";
+    int age = 70;
+    cout << "     /|" << name << endl;
+    cout << "    / |" << age << endl;
+    cout << "   /  |" << endl;
+    cout << "  /   |" << endl;
+}
